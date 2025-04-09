61b829100b5c08590e9cf070dee0528435094100
smunpov1010 <smunpov1010@PC111918.dptoinformatica.iesfuengirola1.net>
2025-04-09 17:35:45 +0200

    commit

--- Agenda/src/agenda/Agenda.java
+++ Agenda/src/agenda/Agenda.java
@@ -7,16 +7,22 @@ import java.sql.Statement;
 public class Agenda {
 
     public static void main(String[] args) {
+        //primero obtenemos la conexion
         Connection conexion = ConexionBaseDatos.getConnection();
-
+        
+        //compruebo si se ha realizado
         if (conexion != null) {
+            
+            //hago las consultas
             try {
+                //insertar
                 String insertar = "INSERT INTO contacto (Nombre, Telefono) VALUES ('Amin',112345688)";
                 String consulta_sql = "SELECT * FROM contacto";
                 Statement stmt = conexion.createStatement();
                 System.out.println(stmt.execute(insertar) ?  "Error al insertar":"Se ha insertado correctamente");
                 ResultSet resultado = stmt.executeQuery(consulta_sql);
-
+                
+                //mostar datos de la base de datos
                 while (resultado.next()) {
                     System.out.println("Id: " + resultado.getInt("id"));
                     System.out.println("Nombre: " + resultado.getNString("Nombre"));
